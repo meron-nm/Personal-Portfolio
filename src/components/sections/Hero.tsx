00c87d3a2aff324c57f556299b1cfdcad0b4a28f
Meron Mulugeta <meronmulugeta114@gmail.com>
2026-09-17 23:35:41 +0300

    docs: update Hero component comment

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -4,7 +4,7 @@ import { styles } from "../../constants/styles";
 import { ComputersCanvas } from "../canvas";
 import { config } from "../../constants/config";
 
-// Hero component
+// Main Hero component
 const Hero = () => {
   return (
     <section className={`relative mx-auto h-screen w-full`}>
